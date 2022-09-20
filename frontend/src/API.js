ca3c605cd8e96bb5ae1b17b9c0fed702bbf6296c
Brandonryder33 <108203422+Brandonryder33@users.noreply.github.com>
2022-09-20 16:34:37 -0500

    Update API.js

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -9,7 +9,8 @@ var baseURL;
 //   baseURL = process.env.REACT_APP_API_BASE_URL;
 // } else
 
-baseURL = "http://127.0.0.1:8000/"
+baseURL = "https://az-amazon-frontend.herokuapp.com/"
+//baseURL = "http://127.0.0.1:8000/"
 
 
 const api = axios.create({
